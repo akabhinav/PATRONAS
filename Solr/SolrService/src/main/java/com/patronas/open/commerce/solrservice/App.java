3138c76f51d2f7cae941fb12bfb54e9eb8990e1c
Abhinav <akabhinav476@gmail.com>
2015-10-04 04:21:45 +0530

    Abhinav Commited

---
+++ Solr/SolrService/src/main/java/com/patronas/open/commerce/solrservice/App.java
@@ -0,0 +1,13 @@
+package com.patronas.open.commerce.solrservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
